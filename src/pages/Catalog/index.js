d3c1b3cecfea39f3c63831fd2594e389127cd0e7
Cassio Glay <cassioglay@gmail.com>
2021-06-11 12:16:03 -0300

    Done Catalog

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Text, View} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+import formatValue from '../../utils/formatValue';
 
 import {
 	Container,
@@ -22,21 +24,21 @@ export default function App() {
 			"id": "1",
 			"title": "Assinatura Trimestral",
 			"image_url":
-			  "https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png",
-			"price": 150
+				"https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png",
+			"price": 15.00
 		},
 		{
 			"id": "2",
 			"title": "Assinatura Trimestral",
 			"image_url":
-			  "https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png",
+				"https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png",
 			"price": 150
 		},
 		{
 			"id": "3",
 			"title": "Assinatura Trimestral",
 			"image_url":
-			  "https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png",
+				"https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png",
 			"price": 150
 		}
 	]);
@@ -45,23 +47,24 @@ export default function App() {
 
 		<Container>
 			<ProductContainer>
-		 		<ProductList
+				<ProductList
 					data={products}
 					keyExtractor={(item) => item.id}
 					ListFooterComponentStyle={{
-                        height:100
-                    }}
-					renderItem={({item}) => 
-					<Product>
-                            <ProductImage source={{ uri: item.image_url}}/>
-                            <ProductTitle>{item.title}</ProductTitle>
-                            <PriceContainer>
-                                <ProductPrice>{item.price}</ProductPrice>
-                                <ProductButton onPress={()=>{}}>
-                                    <ProductButtonText>Adicionar</ProductButtonText>
-                                </ProductButton>
-                            </PriceContainer>
-                        </Product>
+						height: 100
+					}}
+					renderItem={({ item }) =>
+						<Product>
+							<ProductImage source={{ uri: item.image_url }} />
+							<ProductTitle>{item.title}</ProductTitle>
+							<PriceContainer>
+								<ProductPrice>{formatValue(item.price)}</ProductPrice>
+								<ProductButton onPress={() => { }}>
+									<ProductButtonText>Adicionar</ProductButtonText>
+									<FeatherIcon size={30} name="plus-circle" color="#d1d7e9"/>
+								</ProductButton>
+							</PriceContainer>
+						</Product>
 					}
 				/>
 			</ProductContainer>
