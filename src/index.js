67d7603d6f12d728f498091adee8ae8c8503f4ed
cassioglay <cassioglay@gmail.com>
2021-07-27 18:30:18 -0300

    Add Redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import Routes from './routes'
 
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 const App = () => (
-	<>
-		<StatusBar barStyle="light-content" backgroundColor="#312e38"/>
-		<Routes/>
-	</>
+	<Provider store={store}>
+		<StatusBar barStyle="light-content" backgroundColor="#312e38" />
+		<Routes />
+	</Provider>
 )
 export default App;
