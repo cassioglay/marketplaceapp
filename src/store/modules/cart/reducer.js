b08cf44d53ffc58351b3630ac30a8b9a6e0f45ec
cassioglay <cassioglay@gmail.com>
2021-08-06 20:48:05 -0300

    Add actions to remove and update item from Cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -7,6 +7,20 @@ export default function cart(state = [], action) {
                 const { product } = action;
                 draft.push(product);
             });
+        case '@cart/REMOVE':
+            return produce(state, (draft) => {
+                const producIndex = draft.findIndex((p) => p.id === action.id);
+                
+                if(producIndex > 0) draft.splice(producIndex, 1)
+            });
+        case '@cart/UPDATE_AMOUNT_SUCCESS':
+            return produce(state, (draft) => {
+                const producIndex = draft.findIndex((p) => p.id === action.id);
+
+                if(producIndex > 0){
+                    draft[producIndex].amount = Number(action.amount)
+                }
+            });
         default:
             return state;
     }
